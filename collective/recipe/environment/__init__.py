4c5beef8fca64ef885f0bcdaf36f107abcbfe619
Jon Pentland <jon@iomedia.co.uk>
2016-01-19 22:34:14 +0000

    Check for and make safe invalid option names

--- collective/recipe/environment/__init__.py
+++ collective/recipe/environment/__init__.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 """Recipe environment."""
 import os
+import re
+
+match_string = re.compile('.*[:]+.*').match
+
 
 class Recipe(object):
     """zc.buildout recipe"""
 
     def __init__(self, buildout, name, options):
-        self.options = options
-        os.environ.update(options)
+        self.buildout, self.name, self.options = buildout, name, options
         options.update(os.environ)
+        options['UID'] = str(os.getuid())
+        options['GID'] = str(os.getgid())
+
+        for i in options:
+            if match_string(i):
+                i_safe = re.sub(':', '-', i)
+                options[i_safe] = options[i]
+                del options[i]
 
     def install(self):
         """Installer"""
         return tuple()
 
     update = install
-
